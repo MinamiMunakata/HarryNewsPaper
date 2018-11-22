d8cc3d9b2093227ea0647b329d1a83e8f3fd131f
Minami Munakata <june28munazo@gmail.com>
2018-11-22 09:52:57 -0800

    node <- plane <- videoScene

--- HarryNewsPaper/ViewController.swift
+++ HarryNewsPaper/ViewController.swift
@@ -29,8 +29,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
     
-    // Create a session configuration
-    let configuration = ARWorldTrackingConfiguration()
+    // Create a session configuration -> change `world` to `image`
+    let configuration = ARImageTrackingConfiguration()
+    if let trackImage = ARReferenceImage.referenceImages(inGroupNamed: "NewsImages", bundle: Bundle.main) {
+      configuration.trackingImages = trackImage
+      configuration.maximumNumberOfTrackedImages = 1
+    }
     
     // Run the view's session
     sceneView.session.run(configuration)
@@ -44,6 +48,29 @@ class ViewController: UIViewController, ARSCNViewDelegate {
   }
   
   // MARK: - ARSCNViewDelegate
-  
+  func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+    let node = SCNNode()
+    // node <- plane <- videoScene
+    if let imageAnchor = anchor as? ARImageAnchor {
+      let videoNode = SKVideoNode(fileNamed: "harrypotter.mp4")
+      videoNode.play()
+      
+      // 360p -> w:480 h:360, 720p -> w:1080 h:720
+      let videoScene = SKScene(size: CGSize(width: 480, height: 360))
+      videoNode.position = CGPoint(x: videoScene.size.width / 2, y: videoScene.size.height / 2)
+      videoNode.yScale = -1.0
+      videoScene.addChild(videoNode)
+      
+      let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width,
+                           height: imageAnchor.referenceImage.physicalSize.height)
+//      plane.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 0.5)
+      plane.firstMaterial?.diffuse.contents = videoScene
+      let planeNode = SCNNode(geometry: plane)
+      planeNode.eulerAngles.x = -.pi / 2
+      node.addChildNode(planeNode)
+    }
+    
+    return node
+  }
   
 }
