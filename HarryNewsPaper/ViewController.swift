965294184f4fce3f3d4520247d379b1431e54e35
Minami Munakata <june28munazo@gmail.com>
2018-11-22 09:17:36 -0800

    initial commit

---
+++ HarryNewsPaper/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  HarryNewsPaper
+//
+//  Created by minami on 2018-11-22.
+//  Copyright © 2018 宗像三奈美. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+  
+  @IBOutlet var sceneView: ARSCNView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Set the view's delegate
+    sceneView.delegate = self
+    
+    // Show statistics such as fps and timing information
+    sceneView.showsStatistics = true
+    
+    
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    
+    // Create a session configuration
+    let configuration = ARWorldTrackingConfiguration()
+    
+    // Run the view's session
+    sceneView.session.run(configuration)
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    
+    // Pause the view's session
+    sceneView.session.pause()
+  }
+  
+  // MARK: - ARSCNViewDelegate
+  
+  
+}
